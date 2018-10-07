d1ddd7fa711e3e03cb690b55e50ca846b2f09935
Rene Petersen <r.petersen@tfe.nl>
2018-10-07 15:24:04 +0200

    feat: Load Users and Questions to the redux store, Cleanup files

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react'
-import logo from '../logo.svg'
 import '../styles/App.css'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        Starter Code
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default connect()(App);
